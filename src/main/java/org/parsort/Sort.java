b8fc966a37dbc367748119e8ef06decb0b3fdbf0
Dominik Gawlik <dominik.gawlik1@gmail.com>
2021-04-11 17:47:38 +0200

    merge by heap

--- src/main/java/org/parsort/Sort.java
+++ src/main/java/org/parsort/Sort.java
@@ -1,18 +1,29 @@
 package org.parsort;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ForkJoinTask;
 
 public class Sort {
 
+  public static class HeapNode {
+
+    int element;
+    int partIndex;
+    int nextIndex;
+
+    public HeapNode(int element, int partIndex, int nextIndex) {
+      this.element = element;
+      this.partIndex = partIndex;
+      this.nextIndex = nextIndex;
+    }
+  }
+
   public static void sort(int[] arr) {
-    int L1 = 2 * 1024;
+    int L1 = 4 * 1024;
     int parts = (int) Math.ceil(arr.length / (double) L1);
     sortSegments(arr, L1, parts);
-    mergeSegments(arr, L1, parts);
+    mergeSegmentsByHeap(arr, L1, parts);
   }
 
   private static void sortSegments(int[] arr, int L1, int parts) {
@@ -31,7 +42,7 @@ public class Sort {
 
   public static void quickSort(int[] arr, int begin, int end) {
     if (end - begin < 32) {
-      for (int i = begin+1; i <= end; i++) {
+      for (int i = begin + 1; i <= end; i++) {
         int key = arr[i];
         int j = i - 1;
         while (j >= begin && arr[j] > key) {
@@ -65,6 +76,75 @@ public class Sort {
     }
   }
 
+  private static void mergeSegmentsByHeap(int[] arr, int L1, int parts) {
+    long start2 = System.currentTimeMillis();
+
+    int[] buffer = new int[arr.length];
+    int index = 0;
+
+    HeapNode[] heap = new HeapNode[parts];
+    int heapTop = 0;
+    for (int i = 0; i < parts; i++) {
+      heap[heapTop++] = new HeapNode(arr[L1 * i], i, L1 * i + 1);
+      heapifyBottomUp(heap, heapTop - 1);
+    }
+
+    while (heapTop > 0) {
+      HeapNode min = heap[0];
+      buffer[index++] = min.element;
+
+      int limit = Math.min(L1 * (min.partIndex + 1), arr.length);
+      if (min.nextIndex < limit) {
+        heap[0] = new HeapNode(arr[min.nextIndex], min.partIndex,
+            min.nextIndex + 1);
+      } else {
+        heap[0] = heap[heapTop - 1];
+        heapTop--;
+      }
+      heapifyTopDown(heap, heapTop, 0);
+    }
+
+    System.arraycopy(buffer, 0, arr, 0, buffer.length);
+
+    long end2 = System.currentTimeMillis();
+    System.out.println("Merge by heap: " + (end2 - start2) + " ms");
+  }
+
+  private static void heapifyTopDown(HeapNode[] heap, int n, int i) {
+    int l = 2 * i;
+    int r = 2 * i + 1;
+
+    int smallest = i;
+
+    if (l < n && heap[l].element < heap[smallest].element) {
+      smallest = l;
+    }
+
+    if (r < n && heap[r].element < heap[smallest].element) {
+      smallest = r;
+    }
+
+    if (smallest != i) {
+      HeapNode t = heap[i];
+      heap[i] = heap[smallest];
+      heap[smallest] = t;
+      heapifyTopDown(heap, n, smallest);
+    }
+  }
+
+  private static void heapifyBottomUp(HeapNode[] heap, int i) {
+    int p = i / 2;
+
+    if (heap[p].element > heap[i].element) {
+      HeapNode t = heap[i];
+      heap[i] = heap[p];
+      heap[p] = t;
+      if (p > 0) {
+        heapifyBottomUp(heap, p);
+      }
+    }
+  }
+
 
   private static void mergeSegments(int[] arr, int L1, int parts) {
     long start2 = System.currentTimeMillis();
@@ -72,6 +152,7 @@ public class Sort {
     int[] buffer = new int[arr.length];
     int range = 2;
     while (range / parts <= 1) {
+      long start1 = System.currentTimeMillis();
       List<ForkJoinTask<?>> tasks = new ArrayList<>();
 
       for (int i = 0; i < parts; i += range) {
@@ -89,6 +170,9 @@ public class Sort {
 
       ForkJoinTask.invokeAll(tasks).forEach(ForkJoinTask::join);
       range <<= 1;
+
+      long end1 = System.currentTimeMillis();
+      System.out.println("Merge stage: " + (end1 - start1) + " ms");
     }
 
     long end2 = System.currentTimeMillis();
